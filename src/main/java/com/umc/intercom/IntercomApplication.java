37fc2d14a6dbaf37a1aaadf6ae42785c469f030a
이승연 <dltmddus9082@naver.com>
2024-01-12 23:43:26 +0900

    project initial setup

---
+++ src/main/java/com/umc/intercom/IntercomApplication.java
@@ -0,0 +1,13 @@
+package com.umc.intercom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntercomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntercomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umc/intercom/IntercomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umc.intercom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntercomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
