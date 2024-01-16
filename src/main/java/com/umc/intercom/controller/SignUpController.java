419b882f7220b46b9bf763cd2f1ed71ca3ded526
kwon2059 <127478451+kwon2059@users.noreply.github.com>
2024-01-16 22:59:18 +0900

    [FEAT] 회원가입 API 초기설정

---
+++ src/main/java/com/umc/intercom/controller/SignUpController.java
@@ -0,0 +1,10 @@
+package com.umc.intercom.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class SignUpController {
+}
