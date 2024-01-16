26d50c2405753461441fe54faa82299763c5d5e5
kwon2059 <127478451+kwon2059@users.noreply.github.com>
2024-01-16 18:33:16 +0900

    [ADD] Swagger UI 세팅

--- src/main/java/com/umc/intercom/config/SwaggerConfig.java
+++ src/main/java/com/umc/intercom/config/SwaggerConfig.java
@@ -1,2 +1,39 @@
-package com.umc.intercom.config;public class SwaggerConfig {
+package com.umc.intercom.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI UMCStudyAPI() {
+        Info info = new Info()
+                .title("UMC Intercom API")
+                .description("UMC Intercom API 명세서")
+                .version("1.0.0");
+
+        String jwtSchemeName = "JWT TOKEN";
+        // API 요청헤더에 인증정보 포함
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        // SecuritySchemes 등록
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"))
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
 }
