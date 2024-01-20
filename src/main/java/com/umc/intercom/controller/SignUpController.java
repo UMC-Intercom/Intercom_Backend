ba1eb16d74d3d7335b7d199af0ac8e613e55b6e1
kwon2059 <127478451+kwon2059@users.noreply.github.com>
2024-01-20 23:09:21 +0900

    [FEAT] 회원가입 컨트롤러 생성

---
+++ src/main/java/com/umc/intercom/controller/SignUpController.java
@@ -0,0 +1,26 @@
+package com.umc.intercom.controller;
+
+import com.umc.intercom.domain.User;
+import com.umc.intercom.service.SignUpService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class SignUpController {
+    private final SignUpService signUpService;
+
+    public SignUpController(SignUpService signUpService) {
+        this.signUpService = signUpService;
+    }
+    @PostMapping("/signup")
+    public ResponseEntity<Long> signUp(@RequestBody User user) {
+        try {
+            Long userId = signUpService.join(user);
+            return ResponseEntity.ok(userId);
+        } catch (IllegalStateException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+}
+
