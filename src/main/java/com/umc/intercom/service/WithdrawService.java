9d7e760c50bdad24aed79ccfe089beb77fdb8636
DongUk <mywayx1205@naver.com>
2024-01-22 02:05:27 +0900

    [FEAT] 회원탈퇴 서비스 생성

---
+++ src/main/java/com/umc/intercom/service/WithdrawService.java
@@ -0,0 +1,39 @@
+package com.umc.intercom.service;
+
+import com.umc.intercom.domain.User;
+import com.umc.intercom.dto.UserDto;
+import com.umc.intercom.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class WithdrawService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public WithdrawService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void withdraw(UserDto.UserWithdrawRequestDto requestDto) {
+        User user = userRepository.findByEmail(requestDto.getEmail())
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+
+        //Todo : 탈퇴할때 비밀번호 인코딩쪽 문제 해결해야 함
+        /*
+        if(!passwordEncoder.matches(requestDto.getPassword(), user.getPassword())) {
+            throw new BadCredentialsException("비밀번호가 일치하지 않습니다.");
+        }*/
+
+        userRepository.delete(user);
+    }
+}
+
