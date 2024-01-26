9010987c35197d8dc2273f31b68498259181ccf2
kwon2059 <127478451+kwon2059@users.noreply.github.com>
2024-01-27 04:23:35 +0900

    [ADD] CommentDTO 추가

--- src/main/java/com/umc/intercom/dto/CommentDto.java
+++ src/main/java/com/umc/intercom/dto/CommentDto.java
@@ -1,24 +1,27 @@
 package com.umc.intercom.dto;
 
 import com.umc.intercom.domain.Comment;
+import com.umc.intercom.domain.common.enums.AdoptionStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CommentDto {
-    private int id;
+    private Long id;
     private String content;
     private String writer;
+    private AdoptionStatus adoptionStatus;
 
     public static CommentDto toDto(Comment comment) {
         return new CommentDto(
                 comment.getId(),
                 comment.getContent(),
-                comment.getUser().getName()
+                comment.getUser().getNickname(),
+                comment.getAdoptionStatus()
         );
     }
+
 }
