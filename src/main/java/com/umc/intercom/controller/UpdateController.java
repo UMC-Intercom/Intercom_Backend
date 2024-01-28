7346ed3dd8dbba329bc713ffdfb871f6559a28f3
xlooslo <bluearth4587@g.hongik.ac.kr>
2024-01-28 20:54:58 +0900

    [FEAT] Update 기능 구현 중

---
+++ src/main/java/com/umc/intercom/controller/UpdateController.java
@@ -0,0 +1,34 @@
+package com.umc.intercom.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.umc.intercom.dto.UserDto.UserUpdateRequestDto;
+import com.umc.intercom.service.UpdateService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@RestController
+@RequestMapping("/users")
+public class UpdateController {
+    
+    private final UpdateService updateService;
+    
+    @Autowired
+    public UpdateController(UpdateService updateService){
+        this.updateService = updateService;
+    }
+    
+    @PutMapping("/update")
+    public ResponseEntity<Void> updateUser(@RequestBody UserUpdateRequestDto requestDto) {
+        updateService.updateUser(requestDto);
+        return ResponseEntity.noContent().build();
+    }
+    
+}
