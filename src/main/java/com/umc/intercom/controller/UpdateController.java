5eb6825a2e3f6aacd0fd31054c40039ca2348b6d
xlooslo <bluearth4587@g.hongik.ac.kr>
2024-01-29 00:23:26 +0900

    [FIX] Import 에러 수정

--- src/main/java/com/umc/intercom/controller/UpdateController.java
+++ src/main/java/com/umc/intercom/controller/UpdateController.java
@@ -3,12 +3,11 @@ package com.umc.intercom.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.umc.intercom.dto.UserDto.UserUpdateRequestDto;
 import com.umc.intercom.service.UpdateService;
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 
