17d62603548a398da3be7366c654d0b95dd97ee4
kwon2059 <127478451+kwon2059@users.noreply.github.com>
2024-01-29 03:43:35 +0900

    [ADD] Comment 생성, 삭제 완료

--- src/main/java/com/umc/intercom/service/CommentService.java
+++ src/main/java/com/umc/intercom/service/CommentService.java
@@ -1,9 +1,11 @@
 package com.umc.intercom.service;
 
 import com.umc.intercom.domain.Comment;
+import com.umc.intercom.domain.Talk;
 import com.umc.intercom.domain.User;
 import com.umc.intercom.dto.CommentDto;
 import com.umc.intercom.repository.CommentRepository;
+import com.umc.intercom.repository.TalkRepository;
 import com.umc.intercom.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,18 +20,20 @@ import java.util.stream.Collectors;
 public class CommentService {
     private CommentRepository commentRepository;
     private UserRepository userRepository;
-
+    private TalkRepository talkRepository;
 
     @Transactional
-    public CommentDto createComment(CommentDto commentDto, String userEmail) {
-        Optional<User> user = userRepository.findByEmail(userEmail);
+    public CommentDto createComment(CommentDto commentDto) {
+        Optional<Talk> talk = talkRepository.findById(commentDto.getTalkId());
+        Optional<User> user = userRepository.findById(commentDto.getUserId());
         Comment comment = Comment.builder()
                 .content(commentDto.getContent())
                 .adoptionStatus(commentDto.getAdoptionStatus())
+                .talk(talk.orElseThrow(() -> new RuntimeException("Talk not found")))
                 .user(user.orElseThrow(() -> new RuntimeException("User not found")))
                 .build();
 
-        comment.getUser().setNickname(user.get().getNickname());
+//        comment.getUser().setNickname(comment.getUser().getNickname());
 
         Comment savedComment = commentRepository.save(comment);
         return CommentDto.toDto(savedComment);
@@ -37,20 +41,20 @@ public class CommentService {
 
     @Transactional
     public CommentDto updateComment(Long id, CommentDto commentDto) {
-        Comment comment = commentRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 댓글이 없습니다. id=" + id));
+        Comment comment = commentRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 댓글이 없습니다. id= " + id));
         comment.updateContent(commentDto.getContent());
         return CommentDto.toDto(comment);
     }
 
     @Transactional
     public void deleteComment(Long id) {
-        Comment comment = commentRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 댓글이 없습니다. id=" + id));
+        Comment comment = commentRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 댓글이 없습니다. id= " + id));
         commentRepository.delete(comment);
     }
 
     @Transactional(readOnly = true)
-    public List<CommentDto> getComments(Long postId) {
-        List<Comment> comments = commentRepository.findAllByPostId(postId);
+    public List<CommentDto> getComments(Long talkId) {
+        List<Comment> comments = commentRepository.findAllByPostId(talkId);
         return comments.stream().map(CommentDto::toDto).collect(Collectors.toList());
     }
 }
