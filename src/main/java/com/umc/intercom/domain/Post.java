bf6d5fe8032db9506984e17f40a52cc7a729bbad
DongUk <mywayx1205@naver.com>
2024-01-29 20:31:51 +0900

    [ADD] GETTER 추가

--- src/main/java/com/umc/intercom/domain/Post.java
+++ src/main/java/com/umc/intercom/domain/Post.java
@@ -8,6 +8,7 @@ import org.hibernate.annotations.ColumnDefault;
 
 @Entity
 @Getter
+@Setter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
--- src/main/java/com/umc/intercom/domain/PostDetail.java
+++ src/main/java/com/umc/intercom/domain/PostDetail.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.ColumnDefault;
 
 @Entity
 @Getter
+@Setter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
