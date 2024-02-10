f87e1701e80b1a6005dee36d11ccdee52cb88e48
이승연 <dltmddus9082@naver.com>
2024-02-10 23:42:39 +0900

    [ADD] 스크랩 내역 목록 조회 응답 구조 수정

--- src/main/java/com/umc/intercom/dto/InterviewDto.java
+++ src/main/java/com/umc/intercom/dto/InterviewDto.java
@@ -105,41 +105,4 @@ public class InterviewDto {
         }
     }
 
-    @Getter
-    @Builder
-    public static class ScrapResponseDto {
-        private Long id;
-        private String company;
-        private String department;
-        private String year;
-        private String semester;
-        private PostType postType;
-        private int viewCount;
-        private int scrapCount;
-        private String writer;
-        private LocalDateTime createdAt;
-        private LocalDateTime updatedAt;
-
-        public static Page<InterviewDto.ScrapResponseDto> toDtoPage(Page<Post> postPage) {
-            return postPage.map(post -> new InterviewDto.ScrapResponseDto(post.getId(), post.getCompany(), post.getDepartment(), post.getYear(),post.getSemester(),
-                    post.getPostType(), post.getViewCount(), post.getScrapCount(), post.getUser().getNickname(), post.getCreatedAt(), post.getUpdatedAt()));
-        }
-    }
-
-    public static ScrapResponseDto toScrapListDto(Post post) {
-        return ScrapResponseDto.builder()
-                .id(post.getId())
-                .company(post.getCompany())
-                .department(post.getDepartment())
-                .year(post.getYear())
-                .semester(post.getSemester())
-                .postType(post.getPostType())
-                .viewCount(post.getViewCount())
-                .scrapCount(post.getScrapCount())
-                .writer(post.getUser().getNickname())
-                .createdAt(post.getCreatedAt())
-                .updatedAt(post.getUpdatedAt())
-                .build();
-    }
-
 }
