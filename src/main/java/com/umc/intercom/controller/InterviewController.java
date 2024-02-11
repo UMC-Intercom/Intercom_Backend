95b9a0f3b727e6736d36f6f416fc2a4b975d0844
이승연 <dltmddus9082@naver.com>
2024-02-12 03:51:52 +0900

    [FIX] Jackson LocalDate 처리 오류 해결

--- src/main/java/com/umc/intercom/controller/InterviewController.java
+++ src/main/java/com/umc/intercom/controller/InterviewController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
@@ -35,6 +36,7 @@ public class InterviewController {
                                                                              @RequestPart("interviewRequestDto") String interviewRequestDtoString) throws JsonProcessingException {
 
         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
         InterviewDto.InterviewRequestDto interviewRequestDto = objectMapper.readValue(interviewRequestDtoString, InterviewDto.InterviewRequestDto.class);
 
         String userEmail = SecurityUtil.getCurrentUsername();
