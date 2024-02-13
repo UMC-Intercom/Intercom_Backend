fbd7e1a78abe3180347b448e8aa363350ce5173f
kwon2059 <127478451+kwon2059@users.noreply.github.com>
2024-02-14 05:32:08 +0900

    [FEAT] 도메인 생성

---
+++ src/main/java/com/umc/intercom/domain/Company.java
@@ -0,0 +1,21 @@
+package com.umc.intercom.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String logoUrl;
+}
