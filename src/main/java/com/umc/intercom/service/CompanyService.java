2d85ef8c4d9a5cbcad02204a103413b1c03e421a
kwon2059 <127478451+kwon2059@users.noreply.github.com>
2024-02-15 00:29:45 +0900

    [ADD] JSON변환 구현

--- src/main/java/com/umc/intercom/service/CompanyService.java
+++ src/main/java/com/umc/intercom/service/CompanyService.java
@@ -1,18 +1,22 @@
 package com.umc.intercom.service;
 
 import com.umc.intercom.domain.Company;
+import com.umc.intercom.repository.CompanyRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CompanyService {
 
     private final NaverApiClient naverApiClient;
+    private final CompanyRepository companyRepository;
 
-    public CompanyService(NaverApiClient naverApiClient) {
+    public CompanyService(NaverApiClient naverApiClient, CompanyRepository companyRepository) {
         this.naverApiClient = naverApiClient;
+        this.companyRepository = companyRepository;
     }
 
     public Company getCompanyLogo(String name) {
-        return naverApiClient.getCompanyLogo(name);
+        Company company = naverApiClient.getCompanyLogo(name);
+        return companyRepository.save(company);
     }
 }
--- src/main/java/com/umc/intercom/service/NaverApiClient.java
+++ src/main/java/com/umc/intercom/service/NaverApiClient.java
@@ -1,6 +1,8 @@
 package com.umc.intercom.service;
 
 import com.umc.intercom.domain.Company;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
@@ -34,10 +36,15 @@ public class NaverApiClient {
         String url = "https://openapi.naver.com/v1/search/image?query=" + name + "로고 이미지"; // API endpoint
         ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
 
-        System.out.println(response);
-        //Company company = new Company();
+        JSONObject jsonObject = new JSONObject(response.getBody());
+        JSONArray items = jsonObject.getJSONArray("items");
+        String logoUrl = items.getJSONObject(0).getString("link");
 
-        //return company;
-        return null;
+        Company company = Company.builder()
+                .name(name)
+                .logoUrl(logoUrl)
+                .build();
+
+        return company;
     }
 }
